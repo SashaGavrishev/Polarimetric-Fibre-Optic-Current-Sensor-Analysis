80d3fd7c4b2d27e6d187a4d4805f63a446b6822d
sumuli02 <76789665+sumuli02@users.noreply.github.com>
2023-08-31 14:17:07 +0100

    Update remapping.py

--- analysis_tools/remapping.py
+++ analysis_tools/remapping.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 
-def map_ellipse(alpha, beta, phi, cent, x, y):
+def map_ellipse(alpha, beta, phi, cent, s_alpha, s_beta, s_phi, s_cent, x, y):
     """
     Use an ellipse's geometric parameters, to remap ellipse to a unit circle.
 
@@ -17,7 +17,11 @@ def map_ellipse(alpha, beta, phi, cent, x, y):
         beta (float): beta scaling factor
         phi (float): rotation angle [rad]
         cent (np.ndarray): centre vector of ellipse
-        x : ellipse x-data
+        s_alpha (float): 1 standard deviation error of alpha
+        s_beta (float): 1 standard deviation error of beta
+        s_phi (float): 1 standard deviation error of phi [rad]
+        s_cent (np.ndarray): 1 standard deviation error of each centre vector
+        x (np.ndarray): ellipse x-data
         y (np.ndarray): ellipse y-data
 
     Returns:
@@ -28,10 +32,25 @@ def map_ellipse(alpha, beta, phi, cent, x, y):
 
     cent = np.array([[cent[0]], [cent[1]]])
 
-    R_phi = np.array(
-        [[np.cos(phi), -np.sin(phi)], [np.sin(phi), np.cos(phi)]]
-    )
+    R_phi = np.array([[np.cos(phi), - np.sin(phi)],
+                     [np.sin(phi), np.cos(phi)]])
 
-    scale = np.array([[1 / alpha, 0], [0, 1 / beta]])
+    dR_phi_dphi = np.array([[-np.sin(phi), - np.cos(phi)],
+                     [np.cos(phi), -np.sin(phi)]])
+    scale = np.array([[1/alpha, 0], [0, 1/beta]])
 
-    return scale @ R_phi @ (X - cent)
+    result = (scale @ R_phi @ (X - cent))
+
+    df_dphi = scale @ dR_phi_dphi @ (X-cent)
+
+    df_dx0 = scale @ R_phi @ (-1 * np.array([[cent[0][0]], [0]]))
+    df_dy0 = scale @ R_phi @ (-1 * np.array([[0], [cent[1][0]]]))
+
+    df_dscale = -1 * np.array([[1/alpha, 0], [0, 1/beta]]) @ result
+
+    s_scale = np.array([[s_alpha],[s_beta]])
+    error = np.square(df_dphi) * s_phi **2 + np.square(df_dx0) * s_cent[0] **2 + \
+            np.square(df_dy0) * s_cent[1] ** 2 + np.square(df_dscale) * s_scale **2
+    std = np.sqrt(error)
+
+    return result, std
