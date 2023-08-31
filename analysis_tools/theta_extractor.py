1afa517ea4ca8a35792578b503e668a3d40c12ca
sumuli02 <76789665+sumuli02@users.noreply.github.com>
2023-08-31 14:21:46 +0100

    Update theta_extractor.py

--- analysis_tools/theta_extractor.py
+++ analysis_tools/theta_extractor.py
@@ -4,19 +4,41 @@
 import numpy as np
 
 
-def theta_t(time, x_remap, y_remap):
-    """Extract theta(t) from remapped ellipse data."""
+def theta_t(time, x_remap, y_remap, x_std, y_std):
+    """
+    Extract theta(t) from remapped ellipse data.
+
+    Args:
+        time (np.ndarray): time data
+        x_remap (np.ndarray): remapped x-data
+        y_remap (np.ndarray): remapped y-data
+        x_std (np.ndarray): 1 standard deviation error of each 
+            remapped x-data point
+        y_std (np.ndarray): 1 std error of each remapped x-data point
+
+    Returns:
+        time (np.ndarray): time data, unchanged
+        theta (np.ndarray): theta(t) data
+        theta_std (np.ndarray): 1 std error of eac theta(t) data
+    """
     theta_0 = np.arctan2(x_remap[0], y_remap[0])
     theta = [0]
     last = 0
 
     for i, t in enumerate(time):
         if i != 0:
-            theta_t = np.arctan2(x_remap[i], y_remap[i])
-            theta_t = (
-                (theta_t - last + np.pi) % (2 * np.pi) - np.pi + last
-            )
-            last = theta_t
-            theta.append(0.5 * theta_t - 0.5 * theta_0)
+            try:
+                theta_t = (np.arctan2(x_remap[i], y_remap[i]))
+                theta_t = (theta_t-last + np.pi) % (2*np.pi)-np.pi + last
+                last = theta_t
+                theta.append(0.5*theta_t - 0.5*theta_0)
+            except:
+                print(x_remap, y_remap)
+                break
 
-    return time, theta
+    df_dx = 1/(1+ (x_remap/y_remap)**2) * 1/y_remap
+    df_dy = 1/(1+ (x_remap/y_remap)**2) * (-x_remap/y_remap**2)
+    theta_std = np.sqrt(np.square(df_dx) * np.square(x_std) + np.square(df_dy) * np.square(y_std))
+    theta_std = 0.5 * theta_std
+
+    return time, theta, theta_std
